e154792432927abebdafdbf6729d5376b8487a74
Jinx <115119154+JinxMoment@users.noreply.github.com>
2022-10-17 06:03:06 -0600

    New Discord RPC

--- source/Discord.hx
+++ source/Discord.hx
@@ -17,7 +17,7 @@ class DiscordClient
 	{
 		trace("Discord Client starting...");
 		DiscordRpc.start({
-			clientID: "863222024192262205",
+			clientID: "1030686905521668107",
 			onReady: onReady,
 			onError: onError,
 			onDisconnected: onDisconnected
@@ -45,7 +45,7 @@ class DiscordClient
 			details: "In the Menus",
 			state: null,
 			largeImageKey: 'icon',
-			largeImageText: "Psych Engine"
+			largeImageText: "Lua/Haxe Engine"
 		});
 	}
 
