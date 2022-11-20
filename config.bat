cc69009096927478475b65b72cd08e769b1440f1
Jinx 3 <115119154+JinxMoment@users.noreply.github.com>
2022-11-19 18:34:35 -0600

    fuck

---
+++ config.bat
@@ -0,0 +1,20 @@
+@echo off
+color 0a
+cd ..
+@echo on
+echo BUILDING GAME...
+haxelib install lime
+haxelib install openfl
+haxelib install flixel
+haxelib run lime setup flixel
+haxelib run lime setup
+haxelib install flixel-tools
+haxelib run flixel-tools setup
+haxelib install hxCodec
+haxelib install flixel-addons
+haxelib install flixel-ui
+haxelib install hscript
+haxelib install newgrounds
+haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
+haxelib git linc_luajit https://github.com/nebulazorua/linc_luajit
+haxelib git flixel-addons https://github.com/HaxeFlixel/flixel-addons
